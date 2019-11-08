433e9c5ac619054227ade9af748a8c501a50ef92
colombelli <xcolombelli@gmail.com>
2019-11-08 18:43:37 -0300

    python script for svm-rfe ranking implemented, but more investigation on .csv saving (Python) and/or .csv loading (R) must be done

--- Experiments/python-experiments/svm-rfe.py
+++ Experiments/python-experiments/svm-rfe.py
@@ -1,51 +1,61 @@
 #!/usr/bin/python
-
-import time
-import pandas as pd
+from pandas import DataFrame
+from pandas import read_csv
+from time import time
 from sklearn.feature_selection import RFE
 from sklearn.svm import SVR
 import sys
 from argparse import ArgumentParser
 
+
+"""
+    DEALING WITH INITIAL ARGUMENTS
+"""
+
 parser = ArgumentParser()
-parser.add_argument("-f", "--file", dest="filename",
-                    help="csv file name to use", metavar="FILE")
+parser.add_argument("-i", "--input", dest="input",
+                    help="input csv file name to use", metavar="FILE"),
+parser.add_argument("-o", "--output", dest="output",
+                    help="output csv file name to save", metavar="FILE")
+
 args = parser.parse_args()
 
-if (len(sys.argv) != 2):
+if (len(sys.argv) != 5):
     parser.print_help()
     print("\nMissing arguments were found.")
     sys.exit()
 
+
+
+
+
+
 print("Reading dataset...")
-df = pd.read_csv('mergedIQR.csv')
+df = read_csv(args.input, index_col=0)
 
 
 X = df.iloc[0:, 1:-1]
 y = df.iloc[:, -1]
 
-startTime = time.time()
+startTime = time()
 
 estimator = SVR(kernel="linear")
-selector = RFE(estimator, 100, step=1)
-print("Selecting the top 100 most relevant features...")
+selector = RFE(estimator, 1, step=1)
+print("Ranking features...")
 selector = selector.fit(X, y)
 
-endTime = time.time()
+endTime = time()
 elapsedTime = endTime - startTime
 
 
-print("Time passed:", elapsedTime)
+print("Rank finished. Time taken:", elapsedTime)
 
-try:
-    with open("support.txt","w+") as file:
-        file.write(selector.support_)
 
-    with open("ranking.txt","w+") as file:
-        file.write(selector.ranking_)
 
-except:
-    print("\n\n\n")
-    print("suppor", selector.support_)
-    print("\n\n\n")
-    print("ranking", selector.ranking_)
+print("Building CSV...")
+data = {"rank": selector.ranking_}
+rank = DataFrame(data, index=list(X.columns))
+
+
+print("Saving CSV:", args.output)
+rank.to_csv(args.output)
