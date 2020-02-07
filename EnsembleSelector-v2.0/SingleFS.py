fcbfca4c566ec6dac6c22af1bf633720885dd127
colombelli <fcolombelli@inf.ufrgs.br>
2020-02-07 02:45:57 -0300

    SingleFS class added

--- EnsembleSelector-v2.0/SingleFS.py
+++ EnsembleSelector-v2.0/SingleFS.py
@@ -1,16 +1,14 @@
 from Selector import PySelector, RSelector
-from Aggregator import Aggregator
 from DataManager import DataManager
 from Constants import *
 
-class Homogeneous:
+class SingleFS:
     
     # fs_method: a tuple (script name, language which the script was written, .rds output name)
-    def __init__(self, data_manager:DataManager, fs_method, aggregator):
+    def __init__(self, data_manager:DataManager, fs_method):
 
         self.dm = data_manager
         self.fs_method = self.__generate_fselector_object(fs_method)
-        self.aggregator = Aggregator(aggregator, self.dm)
 
 
         
@@ -29,22 +27,19 @@ class Homogeneous:
 
         for i in range(self.dm.num_folds):
             print("\n\n################# Fold iteration:", i+1, "#################")
+            
             self.dm.current_fold_iteration = i
-            self.dm.update_bootstraps()
-
-            rankings = []
-            for j, (bootstrap, _) in enumerate(self.dm.current_bootstraps):
-                print("\n\nBootstrap: ", j+1, "\n")
-                
-                output_path = self.dm.get_output_path(i, j)
-                bootstrap_data = self.dm.pd_df.loc[bootstrap]
-                rankings.append(self.fs_method.select(bootstrap_data, output_path))
-
-                
-            print("\nAggregating rankings...")
             output_path = self.dm.get_output_path(fold_iteration=i)
-            aggregation = self.aggregator.aggregate(rankings)
-            self.dm.save_encoded_ranking(aggregation, 
+
+            training_indexes, _ = self.dm.get_fold_data()
+            training_data = self.dm.pd_df.loc[training_indexes]
+
+            ranking = self.fs_method.select(training_data, output_path)
+
+            # in order to reuse Evaluator class, we need an AGGREGATED_RANKING_FILE_NAME.rds
+            # accessible inside each fold iteration folder, so we simply resave the only
+            # ranking we have with the appropriate name
+            self.dm.save_encoded_ranking(ranking, 
                                         output_path+AGGREGATED_RANKING_FILE_NAME)
             
         return
--- EnsembleSelector-v2.0/main.py
+++ EnsembleSelector-v2.0/main.py
@@ -2,6 +2,7 @@ from DataManager import DataManager
 from Hybrid import Hybrid
 from Heterogeneous import Heterogeneous
 from Homogeneous import Homogeneous
+from SingleFS import SingleFS
 from Evaluator import Evaluator
 import rpy2.robjects.packages as rpackages
 
@@ -9,7 +10,7 @@ import rpy2.robjects.packages as rpackages
 #dataset_path = "/home/colombelli/Documents/THCA/iqrSelectedGenes.rds"
 #results_path = "/home/colombelli/Documents/bioinformatics-ml/EnsembleSelector-v2.0/resultsTHCA/"
 dataset_path = "/home/colombelli/Documents/datasets/thyroid_log2.rds"
-results_path = "/home/colombelli/Documents/geoDE_homo/"
+results_path = "/home/colombelli/Documents/single2/"
 
 
 rpackages.importr('CORElearn')
@@ -19,8 +20,8 @@ rpackages.importr('FSelector')
 
 seed = 42
 #num_bootstraps = 2
-#num_bootstraps = 0
-num_bootstraps = 50
+num_bootstraps = 0
+#num_bootstraps = 50
 num_folds = 3
 
 
@@ -39,9 +40,13 @@ dm = DataManager(results_path, dataset_path, num_bootstraps, num_folds, seed)
 
 #ensemble = Hybrid(dm, fs_methods, aggregator, aggregator)
 #ensemble = Heterogeneous(dm, fs_methods, aggregator)
-homo_method=("geoDE", "python", "gd")
-ensemble = Homogeneous(dm, homo_method, aggregator)
-ensemble.select_features()
+#homo_method=("geoDE", "python", "gd")
+#ensemble = Homogeneous(dm, homo_method, aggregator)
+#ensemble.select_features()
+
+method = ("gain-ratio", "r", "gr")#("geoDE", "python", "gd")
+single_fs = SingleFS(dm, method)
+single_fs.select_features()
 
 print("\n\nStarting evaluation process...")
 ev = Evaluator(dm, [0.0001, 0.1, 0.5, 1, 2, 5])
