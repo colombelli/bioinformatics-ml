0707de88941593bca6aa32070360a29210a246d1
colombelli <fcolombelli@inf.ufrgs.br>
2020-02-07 04:29:10 -0300

    reliefF python algorithm added (much faster and accurate than its R version)

--- EnsembleSelector-v2.0/main.py
+++ EnsembleSelector-v2.0/main.py
@@ -9,8 +9,10 @@ import rpy2.robjects.packages as rpackages
 
 #dataset_path = "/home/colombelli/Documents/THCA/iqrSelectedGenes.rds"
 #results_path = "/home/colombelli/Documents/bioinformatics-ml/EnsembleSelector-v2.0/resultsTHCA/"
-dataset_path = "/home/colombelli/Documents/datasets/thyroid_log2.rds"
-results_path = "/home/colombelli/Documents/single2/"
+#dataset_path = "/home/colombelli/Documents/datasets/thyroid_log2.rds"
+#results_path = "/home/colombelli/Documents/single2/"
+dataset_path = "/home/colombelli/Documents/datasets/iqrSelectedGenesAllSamples.rds"
+results_path = "/home/colombelli/Documents/BRCA_Hybrid_mean_mean/"
 
 
 rpackages.importr('CORElearn')
@@ -20,36 +22,52 @@ rpackages.importr('FSelector')
 
 seed = 42
 #num_bootstraps = 2
-num_bootstraps = 0
-#num_bootstraps = 50
-num_folds = 3
+#num_bootstraps = 0
+num_bootstraps = 5
+num_folds = 5
 
 
 fs_methods = [
-    ("gain-ratio", "r", "gr"),
-    ("geoDE", "python", "gd"),
-    ("symmetrical-uncertainty", "r", "su")#,
-    #("relief", "r", "rf"),
-    #("oneR", "r", "or"),
-    #("svm_rfe", "python", "svmrfe")
+    #("gain-ratio", "r", "gr"),
+    #("symmetrical-uncertainty", "r", "su"),
+    ("reliefF", "python", "rf"),
+    ("geoDE", "python", "gd")#,
+    #("oneR", "r", "or")#,
 ]
 
 aggregator = "mean"
 
 dm = DataManager(results_path, dataset_path, num_bootstraps, num_folds, seed)
 
-#ensemble = Hybrid(dm, fs_methods, aggregator, aggregator)
+ensemble = Hybrid(dm, fs_methods, aggregator, aggregator)
 #ensemble = Heterogeneous(dm, fs_methods, aggregator)
 #homo_method=("geoDE", "python", "gd")
 #ensemble = Homogeneous(dm, homo_method, aggregator)
-#ensemble.select_features()
 
-method = ("gain-ratio", "r", "gr")#("geoDE", "python", "gd")
-single_fs = SingleFS(dm, method)
-single_fs.select_features()
+"""
+from time import time
+st = time()
+ensemble.select_features()
+ed = time()
+
+try:
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+
+    print("\n\n!!!!! TIME TAKEN:", 
+            "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds), 
+            "!!!!!")
+except:
+    pass
+"""
+#method = ("gain-ratio", "r", "gr")#("geoDE", "python", "gd")
+#single_fs = SingleFS(dm, method)
+#single_fs.select_features()
 
 print("\n\nStarting evaluation process...")
-ev = Evaluator(dm, [0.0001, 0.1, 0.5, 1, 2, 5])
+#ths = [0.1, 0.2, 0.3, 0.5, 0.8, 1, 1.5, 2, 3, 5]
+ths = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07]
+ev = Evaluator(dm, ths)
 aucs, stabilities = ev.evaluate_final_rankings()
 
 print("\n\nAUCs:")
