0d00b575ef1b31b9784de123c441ab89f0cd66d0
colombelli <xcolombelli@gmail.com>
2020-07-09 21:21:12 -0300

    processing rodrigo datasets

--- engine/__main__.py
+++ engine/__main__.py
@@ -2,5 +2,5 @@ from engine import root
 from engine import temp_script_reeval
 
 if __name__ == '__main__':
-    #root.run()
-    temp_script_reeval.run()
+    root.run()
+    #temp_script_reeval.run()
