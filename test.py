ef0ec0a00169054c5137b112ec848198c550206f
colombelli <xcolombelli@gmail.com>
2020-07-31 21:33:51 -0300

    added script for testing with new processed BRCA datasets

--- test.py
+++ test.py
@@ -1,65 +1,112 @@
 import rpy2.robjects.packages as rpackages
 from efsassembler.Experiments import Experiments
+from copy import deepcopy
 
 rpackages.quiet_require('FSelectorRcpp')
 
-micarr_npn = "/home/colombelli/Documents/datasets/assembler/brca_micarr_npn.csv"
+intersect = "/home/colombelli/Documents/up_files/brca_rnaseq_genes_intersec.csv"
+no_intersect = "/home/colombelli/Documents/up_files/brca_rnaseq_npn.csv"
+datasets = [intersect, no_intersect]
 
-sin={
-        "type": "sin",
-        "thresholds": [5, 10, 20],
-        "seed": 14,
-        "folds": 5,
-        "selectors": [("wx", "python", "wx")],
-        "datasets": [micarr_npn]
-    }
+
+relieff = ("reliefF", "python", "rf"),
+geode = ("geoDE", "python", "gd"),
+gr = ("gain-ratio", "r", "gr"),
+su = ("symmetrical-uncertainty", "r", "su"),
+wx = ("wx", "python", "wx")
+
+all_fs = [relieff, geode, gr, su, wx]
+ths = [1, 5, 10, 15, 25, 50, 75, 100, 150, 200]
+seed = 42
+k = 5
+num_bs = 50
+
+
+# -----------------------------------
+#       HETEROGENEOUS
+# -----------------------------------
 
 het={
         "type": "het",
-        "thresholds": [5, 10, 20],
-        "seed": 14,
-        "folds": 5,
+        "thresholds": ths,
+        "seed": seed,
+        "folds": k,
         "aggregators": ["borda"],
-        "selectors": [("wx", "python", "wx"), ("gain-ratio", "r", "gr"), ("geoDE", "python", "gd")],
-        "datasets": [micarr_npn]
+        "selectors": all_fs,
+        "datasets": datasets
     }
 
-hom={
-        "type": "hom",
-        "thresholds": [5, 10, 20],
-        "bootstraps": 3,
-        "seed": 14,
-        "folds": 3,
-        "aggregators": ["borda"],
-        "selectors": [("wx", "python", "wx")],
-        "datasets": [micarr_npn]
-    }
+
+# -----------------------------------
+#       HYBRID
+# -----------------------------------
 
 hyb1={
         "type": "hyb",
-        "thresholds": [5, 10, 20],
-        "bootstraps": 3,
-        "seed": 14,
-        "folds": 3,
+        "thresholds": ths,
+        "bootstraps": num_bs,
+        "seed": seed,
+        "folds": k,
         "aggregators": ["borda", "borda"],
-        "selectors": [("wx", "python", "wx"), ("gain-ratio", "r", "gr"), ("geoDE", "python", "gd")],
-        "datasets": [micarr_npn]
+        "selectors": all_fs,
+        "datasets": datasets
 }
 
 hyb2={
         "type": "hyb",
-        "thresholds": [5, 10, 20],
-        "bootstraps": 3,
-        "seed": 14,
-        "folds": 3,
+        "thresholds": ths,
+        "bootstraps": num_bs,
+        "seed": seed,
+        "folds": k,
         "aggregators": ["stb_weightened_layer1", "borda"],
-        "selectors": [("wx", "python", "wx"), ("gain-ratio", "r", "gr"), ("geoDE", "python", "gd")],
-        "datasets": [micarr_npn]
+        "selectors": all_fs,
+        "datasets": datasets
 }
 
-experiments = [hyb1, hyb2]
 
-results_path = "/home/colombelli/Documents/Exptest"
+
+# -----------------------------------
+#     HOMOGENEOUS and SINGLE
+# -----------------------------------
+
+hom_exps = []
+sin_exps = []
+
+hom_base={
+        "type": "hom",
+        "thresholds": ths,
+        "bootstraps": num_bs,
+        "seed": seed,
+        "folds": k,
+        "aggregators": ["borda"],
+        "datasets": datasets
+    }
+
+sin_base={
+        "type": "sin",
+        "thresholds": ths,
+        "seed": seed,
+        "folds": k,
+        "datasets": datasets
+    }
+
+
+for sel in all_fs:
+        hom_exp = deepcopy(hom_base)
+        hom_exp["selectors"] = [sel]
+        hom_exps.append(hom_exp)
+
+        sin_exp = deepcopy(sin_base)
+        sin_exp["selectors"] = [sel]
+        sin_exps.append(sin_exp)
+
+
+
+
+experiments = [het, hyb1, hyb2]
+experiments += hom_exps + sin_exps
+
+results_path = "/home/colombelli/Documents/Experiments31_jul"
 
 print("STARTING PROCESS!!!")
 exp = Experiments(experiments, results_path)
